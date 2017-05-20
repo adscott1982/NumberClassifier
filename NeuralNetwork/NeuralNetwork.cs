6552bca6ba9e7804a166663df116e834ba7ea598
Andrew Scott <adscott1982@googlemail.com>
2017-05-20 20:13:56 +0100

    Initial commit.

---
+++ NeuralNetwork/NeuralNetwork.cs
@@ -0,0 +1,18 @@
+﻿namespace NeuralNetwork
+{
+    public class NeuralNetwork
+    {
+        private int inputNodes;
+        private int hiddenNodes;
+        private int outputNodes;
+        private double learningRate;
+
+        public NeuralNetwork(int inputNodes, int hiddenNodes, int outputNodes, double learningRate)
+        {
+            this.inputNodes = inputNodes;
+            this.hiddenNodes = hiddenNodes;
+            this.outputNodes = outputNodes;
+            this.learningRate = learningRate;
+        }
+    }
+}
